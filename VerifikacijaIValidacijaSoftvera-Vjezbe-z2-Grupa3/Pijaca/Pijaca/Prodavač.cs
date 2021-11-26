3ee14c3a5d4cf3db3c7dc9c36c523281590f4ae3
ahusicic1 <ahusicic1@etf.unsa.ba>
2021-11-26 18:49:24 +0100

    Testiranje metode RegistrujPromet

--- VerifikacijaIValidacijaSoftvera-Vjezbe-z2-Grupa3/Pijaca/Pijaca/Prodavač.cs
+++ VerifikacijaIValidacijaSoftvera-Vjezbe-z2-Grupa3/Pijaca/Pijaca/Prodavač.cs
@@ -76,7 +76,7 @@ namespace Pijaca
         public void RegistrujPromet(string šifra, double promet, DateTime početak, DateTime kraj)
         {
             if (šifra != sigurnosniKod) throw new InvalidOperationException("Poslana šifra se ne poklapa sa kodom!");
-            else if (ukupniPromet == 0 && (početak.AddMonths(1) > kraj)) Aktivnost = false;
+            else if (ukupniPromet == 0 && (početak.AddMonths(1) <= kraj)) Aktivnost = false;
             else if (promet > 1000 && (početak - kraj).TotalDays < 2) throw new InvalidOperationException("Neispravno uneseni podaci!");
             else ukupniPromet += promet; 
         }
--- VerifikacijaIValidacijaSoftvera-Vjezbe-z2-Grupa3/Pijaca/TestProjectG3/UnitTest1.cs
+++ VerifikacijaIValidacijaSoftvera-Vjezbe-z2-Grupa3/Pijaca/TestProjectG3/UnitTest1.cs
@@ -2,7 +2,6 @@
 using Pijaca;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 
 namespace TestProjectG3
 {
@@ -10,14 +9,15 @@ namespace TestProjectG3
     public class UnitTest1
     {
         [TestMethod]
-        public void Test1GenerisanjeSifreDomaciIStrani()
+        public void Test1GenerisanjeSifreDomaci()
         {
-            //Mirza Kadrić
-            Proizvod p = new Proizvod(Namirnica.Povrće, "Luk", 20, DateTime.Now, 1.5, true);
-            Assert.AreEqual(p.ŠifraProizvoda, "387 - 1000 - 9");
+          
+        }
 
-            p = new Proizvod(Namirnica.Voće, "Jagoda", 30, DateTime.Now, 2, false);
-            Assert.AreEqual(p.ŠifraProizvoda, "111 - 1001 - 5");
+        [TestMethod]
+        public void TestGenerisanjeSifreStrani()
+        {
+
         }
 
         [TestMethod]
@@ -29,33 +29,47 @@ namespace TestProjectG3
             prodavac1.Aktivnost = false;
             Prodavač prodavac2 = new Prodavač("Drugi", "1235", new DateTime(2021, 2, 10), 50);
             prodavac2.Aktivnost = true;
-            Prodavač prodavac3 = new Prodavač("Treci", "1236", new DateTime(2021, 5, 1), 2000);
-            prodavac3.Aktivnost = true;
-            Prodavač prodavac4 = new Prodavač("Cetvrti", "1237", new DateTime(2021, 8, 1), 200000);
-            prodavac4.Aktivnost = false;
             Proizvod proizvod = new Proizvod(Namirnica.Meso, "Piletina", 20, DateTime.Now, 10, false);
-
             trznica.RadSaProdavačima(prodavac1, "Dodavanje");
             trznica.RadSaProdavačima(prodavac2, "Dodavanje");
-            trznica.RadSaProdavačima(prodavac3, "Dodavanje");
-            trznica.RadSaProdavačima(prodavac4, "Dodavanje");
-
             List<Proizvod> listaProizvoda = new List<Proizvod>() { proizvod };
-
             trznica.OtvoriŠtand(prodavac1, listaProizvoda, new DateTime(2021, 12, 15));
             trznica.OtvoriŠtand(prodavac2, listaProizvoda, new DateTime(2021, 5, 20));
-            trznica.OtvoriŠtand(prodavac3, listaProizvoda, new DateTime(2022, 1, 20));
-            trznica.OtvoriŠtand(prodavac4, listaProizvoda, new DateTime(2022, 3, 21));
-            
             trznica.ZatvoriSveNeaktivneŠtandove();
+            CollectionAssert.AreEquivalent(trznica.Štandovi, new List<Štand>());
 
-            Štand stand3 = new Štand(prodavac3, new DateTime(2022, 1, 20), listaProizvoda);
-            Štand stand4 = new Štand(prodavac4, new DateTime(2022, 3, 21), listaProizvoda);
-            List<Štand> trazeniStandovi = new List<Štand>() { stand3, stand4 };
+        }
 
-            Assert.AreEqual(2, trznica.Štandovi.Count);
-            Assert.AreEqual(stand3.Prodavač.Ime, trznica.Štandovi[0].Prodavač.Ime);
-            Assert.AreEqual(stand4.Prodavač.Ime, trznica.Štandovi[1].Prodavač.Ime);
+
+        //Adna Husičić
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestRegistrujPrometIzuzetak1()
+        {
+            Prodavač p = new Prodavač("prodavac", "000", DateTime.Now, 0);
+            p.RegistrujPromet("123", 0, DateTime.Now, DateTime.Now);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestRegistrujPrometIzuzetak2()
+        {
+            Prodavač p = new Prodavač("prodavac", "000", DateTime.Now, 0);
+            p.RegistrujPromet("123", 3000, DateTime.Now.AddDays(-1), DateTime.Now);
+        }
+
+        [TestMethod]
+        public void TestRegistrujPrometAktivnost()
+        {
+            Prodavač prodavac1 = new Prodavač("prodavac", "000", DateTime.Parse("01/07/2021"), 0);
+            Prodavač prodavac2 = new Prodavač("prodavac", "001", DateTime.Parse("01/07/2021"), 0);
+            Assert.IsTrue(prodavac1.Aktivnost);
+ 
+            prodavac1.RegistrujPromet("000", 0, DateTime.Parse("01/07/2021"), DateTime.Parse("01/09/2021"));
+            prodavac2.RegistrujPromet("001", 1000, DateTime.Parse("01/07/2021"), DateTime.Parse("10/07/2021"));
+            Assert.IsFalse(prodavac1.Aktivnost);
+            Assert.IsTrue(prodavac2.Aktivnost);
+            Assert.AreEqual(prodavac2.UkupniPromet, 1000);
 
         }
 
